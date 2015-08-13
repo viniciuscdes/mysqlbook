b976295062bf0984543e913aea1f78376b8199b4
Vinicius Carvalho <carvalhoo1988@gmail.com>
2015-08-13 15:03:28 -0300

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -9,3 +9,23 @@
 Arquivo: functions.sql
 
 */
+
+mysql>delimiter $$
+mysql> create function rt_nome_cliente(vn_numeclien int)
+			returns varchar(50)
+			
+			begin
+		
+				declare nome varchar(50);
+			
+				select c_nomeclien  into nome
+				  from comclien
+				where n_numeclien = vn_numeclien;
+			 
+				return nome;
+			 
+			end $$
+mysql> delimiter ;
+
+
+ 
